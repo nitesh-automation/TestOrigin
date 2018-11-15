a77683524934f0bc4e242a21e917a18b9a784b7e
Nitesh <Nitesh@NiteshMain.home>
2018-11-14 20:00:53 -0600

    Second commit

--- Test1/src/pkg1/TestRun.java
+++ Test1/src/pkg1/TestRun.java
@@ -4,6 +4,7 @@ public class TestRun {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Added after Git installation");
+		System.out.println("Added after initial commit");
 	}
 
 }
